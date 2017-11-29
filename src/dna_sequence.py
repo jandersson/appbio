2c7bf83cc614d99fea01056a02c33437f6e56b1a
Jonas Andersson <jonas@andersson.io>
2017-11-29 17:57:48 +0100

    Compute GC content

--- src/dna_sequence.py
+++ src/dna_sequence.py
@@ -1,14 +1,35 @@
 class DnaSequence(object):
-    def __init__(self, sequence=""):
-        self._sequence = sequence
+    def __init__(self, sequence=''):
+        self.sequence = sequence
 
     @property
     def sequence(self):
         return self._sequence
 
+    @sequence.setter
     def sequence(self, new_sequence):
-        self._sequence = new_sequence
-        self.compute_gc()
+        self._sequence = new_sequence.upper().strip()
+        self._compute_gc()
 
-    def compute_gc(self): 
-        pass
+    def _compute_gc(self): 
+        if self._sequence == "":
+            self.gc_content = 0
+        else:
+            seq = self._sequence
+            cytosine_count = seq.count('C')
+            guanine_count = seq.count('G')
+            adanine_count = seq.count('A')
+            thymine_count = seq.count('T')
+            self.gc_content = (guanine_count + cytosine_count) / (cytosine_count + guanine_count + adanine_count + thymine_count)
+
+    def __repr__(self):
+        return self._sequence
+
+if __name__ == '__main__':
+    s = DnaSequence()
+    s.sequence = 'GATTACA'
+    print(s)
+    print(s.gc_content)
+    s = DnaSequence('GCAT')
+    print(s.sequence)
+    print(s.gc_content)
