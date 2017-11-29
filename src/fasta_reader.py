eac10ce391f1c818b012925a2684170aa51d5f47
Jonas Andersson <jonas@andersson.io>
2017-11-29 05:18:23 +0100

    Add docstrings, formatting

--- src/fasta_reader.py
+++ src/fasta_reader.py
@@ -1,16 +1,19 @@
 from sequence_reader import SequenceReader
 
+
 class FastaReader(SequenceReader):
     def is_sequence_name_line(self, line):
         return line[0] == '>'
 
     def get_sequence_name(self, line):
-        if not self.is_sequence_name_line(line): 
-            return 
-    
+        """If the line is a sequence name, process and return"""
+        if not self.is_sequence_name_line(line):
+            return
+
         return line[1:].rstrip()
-    
+
     def get_sequence(self, line):
+        """Process sequence line and return it"""
         return line.rstrip()
 
     def read_sequences(self):
@@ -24,11 +27,11 @@ class FastaReader(SequenceReader):
 
 if __name__ == '__main__':
     import os
-    reader = FastaReader()
     data_path = os.path.join(os.path.dirname(__file__), os.pardir, 'data')
-    test_files = ['an_exon.fa']
+    test_files = ['an_exon.fa', 'translationtest.dna']
     for data_file in test_files:
+        reader = FastaReader()
         data = os.path.join(data_path, data_file)
-        reader.read_file(data)        
+        reader.read_file(data)
         reader.print_sequence_names()
         print(reader.sequences)
