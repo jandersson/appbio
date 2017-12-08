d09d1c968f36b70184f02ebf9317d5c7cae618c1
jandersson <jonas@andersson.io>
2017-12-08 10:31:56 +0100

    Using pexpect because subprocess is a nightmare with phylip

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -8,6 +8,9 @@ def to_seqboot(sequences):
 
 if __name__ == '__main__':
     import argparse
+    import tempfile
+    import pexpect
+
     from common import is_valid_file
     from fasta_reader import FastaReader
     from fasta_writer import to_fasta
@@ -15,10 +18,30 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run Phylip bootstrap analysis on protein alignments')
     parser.add_argument('file', type=lambda f: is_valid_file(f, parser),
                         help='Protein alignment file in FASTA format')
-    parser.add_argument('num_replicates', type=int, help='Number of bootstrap replicates to run')
-    args = parser.parse_args(['/home/jonas/appbio/data/small1.fa', '5'])
+    parser.add_argument('num_replicates', help='Number of bootstrap replicates to run')
+    args = parser.parse_args(['/home/jonas/appbio/data/small1.fa', '1000'])
 
     reader = FastaReader()
     reader.read_file(args.file)
     seq_string = to_seqboot(reader.sequences)
+    infile = tempfile.NamedTemporaryFile()
+    infile.write(seq_string.encode())
+    infile.seek(0)
 
+    seqboot = pexpect.spawn('seqboot', encoding='utf-8')
+    seqboot.expect(str('Please enter a new file name'))
+    seqboot.sendline(infile.name)
+    seqboot.expect('Y to accept')
+    seqboot.sendline("I")
+    seqboot.sendline("R")
+    seqboot.expect('Number of replicates')
+    seqboot.sendline("1000")
+    seqboot.expect('Y to accept')
+    seqboot.sendline('Y')
+    seqboot.expect('Random number seed')
+    seqboot.sendline('1001')
+    i = seqboot.expect([pexpect.EOF, 'please type R, A, F, or Q'])
+    if i == 1:
+        seqboot.sendline("R")
+    infile.close()
+    
